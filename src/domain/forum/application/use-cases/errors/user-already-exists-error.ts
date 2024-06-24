63436a3cf9a1a663f754a267196dd05c1dfe09c1
Marcel Brasileiro <brasileirodev@gmail.com>
2024-06-24 18:41:10 -0300

    Revert "Importando projeto da versão do lucas"
    This reverts commit 0a29c816def4291fa9fdfc646ad1e0eb2aab7350.

--- src/domain/forum/application/use-cases/errors/user-already-exists-error.ts
+++
@@ -1,7 +0,0 @@
-import { UseCaseError } from '@/core/errors/use-case-error'
-
-export class UserAlreadyExistsError extends Error implements UseCaseError {
-  constructor(identifier: string) {
-    super(`User "${identifier}" already exists.`)
-  }
-}
